239ee79568bd2ecdf61a4494d5304c30a9c8e54c
Dot <Dot@DESKTOP-V6BIU4B>
2019-03-08 11:04:27 +0330

    new client webapi

--- AngularWebApi/App_Start/WebApiConfig.cs
+++ AngularWebApi/App_Start/WebApiConfig.cs
@@ -11,16 +11,12 @@ namespace AngularWebApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
 
             // Web API routes
             config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
         }
     }
 }
--- AngularWebApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace AngularWebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
