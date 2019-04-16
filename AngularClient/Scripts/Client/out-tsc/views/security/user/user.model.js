a761728c3b52f865bb3f0826c2aa834e42be68d9
Dot <Dot@DESKTOP-V6BIU4B>
2019-04-16 06:10:43 +0430

    update angular in AngularClient

---
+++ AngularClient/Scripts/Client/out-tsc/views/security/user/user.model.js
@@ -0,0 +1,10 @@
+var UserModel = /** @class */ (function () {
+    function UserModel() {
+        this.Name = "";
+        this.UserName = "";
+        this.Password = null;
+    }
+    return UserModel;
+}());
+export { UserModel };
+//# sourceMappingURL=user.model.js.map
---
+++ AngularClient/Scripts/Client/out-tsc/views/security/user/userInfo.component.js
@@ -0,0 +1,24 @@
+import * as tslib_1 from "tslib";
+import { Component } from '@angular/core';
+import { UserModel } from './user.model';
+var UserInfoComponent = /** @class */ (function () {
+    function UserInfoComponent() {
+        this.User = new UserModel();
+    }
+    UserInfoComponent.prototype.ngOnInit = function () {
+        // this.dataService.get('Home').subscribe(data => {
+        //   console.log(data);
+        // }, error => {
+        //   console.error(error)
+        // })
+    };
+    UserInfoComponent = tslib_1.__decorate([
+        Component({
+            templateUrl: './userInfo.view.html'
+        }),
+        tslib_1.__metadata("design:paramtypes", [])
+    ], UserInfoComponent);
+    return UserInfoComponent;
+}());
+export { UserInfoComponent };
+//# sourceMappingURL=userInfo.component.js.map
